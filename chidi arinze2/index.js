181cf182110019c419bf32e77d8ef78ef9c30083
chidi arinze <86808937+Akpi1@users.noreply.github.com>
2023-03-30 23:31:23 +0100

    Update index.js

--- chidi arinze2/index.js
+++ chidi arinze2/index.js
@@ -1,23 +1,32 @@
 const hamburger = document.querySelector('.hamburger')
-const nav = document.querySelector('.smallNav')
+const nav = document.querySelector('.smallNav');
+const bHamburger = document.querySelector('.bHamburger')
 
 
 
 hamburger.addEventListener('click' , function(){
 
-    const slider = nav.getAttribute('data-visible')
+    const slider = nav.getAttribute('data-visible');
+    const slider2 = bHamburger.getAttribute('aria-expanded');
+    const slider3 = hamburger.getAttribute('isClicked')
 
-    if (slider == 'false'){
+    if (slider == 'false' && slider2 == 'false' && slider3 == 'false'){
         nav.setAttribute('data-visible', true) 
+        bHamburger.setAttribute('aria-expanded', true) 
+        hamburger.setAttribute('isClicked', true) 
+        
     }else{
         nav.setAttribute('data-visible' , false)
-        // nav.setAttribute('data-visible' , false)  
+        bHamburger.setAttribute('aria-expanded' , false)
+        hamburger.setAttribute('isClicked', false) 
+          
     }
     
 })
 // ============= close navbar on click==========================================
 const closeBtn = document.querySelectorAll('.close-btn')
-closeBtn.forEach(close=>{
+
+closeBtn.forEach(close => {
      const slider = nav.getAttribute('data-visible')
 
     close.addEventListener('click',function(){
@@ -27,10 +36,26 @@ closeBtn.forEach(close=>{
     })
 })
 // ================================================
+//============= project toggle ==========================
+const projectToggle = document.querySelector('.toggleProject')
+const plusOpen = document.querySelector('.plus')
+const plusClose = document.querySelector('.minus')
+const landing_customWrapper = document.querySelector('.landing_customWrapper')
 
 
+plusOpen.addEventListener('click' , function() {
+    plusOpen.style.display = 'none'
+    plusClose.style.display = 'inline'
+    landing_customWrapper.style.display = 'block'
+})
+plusClose.addEventListener('click' , function() {
+    plusClose.style.display = 'none'
+    plusOpen.style.display = 'inline'
+    landing_customWrapper.style.display = 'none'
+})
 
-// ============
+
+// ============ end of project toggle ===========
 
 // ==================== intersection observer for small devices ==================
 const hide = document.querySelectorAll('.hidden1')
